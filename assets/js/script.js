0063b37a1b7f7ca5c8b303cc646ce039a945618b
Lily <nathdubas@gmail.com>
2019-10-28 10:06:54 +0100

    focusout

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+function myFunction() {
+  var x = document.getElementById("lastname"); // Sélectionne l'élement a partir de son id
+  alert("Merci de votre participation"); //Affiche la boite de dialogue
+}
